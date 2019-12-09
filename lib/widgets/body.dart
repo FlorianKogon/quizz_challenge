cfa2eed8475492c910e123f5943198fa8efb8a9c
Florian Kogon <F.Kogon@gmail.com>
2019-12-09 14:33:41 +0100

    "update"

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_challenge/quizz.dart';
+import 'package:quizz_challenge/widgets/custom_text.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -16,9 +17,10 @@ class _BodyState extends State<Body> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Card(
+            elevation: 10.0,
             child: Container(
-              width: MediaQuery.of(context).size.width / 1.5,
-              height: 250,
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.width * 0.8,
               child: Image.asset("assets/cover.jpg",
                 fit: BoxFit.cover,
               ),
@@ -26,17 +28,15 @@ class _BodyState extends State<Body> {
           ),
           RaisedButton(
             onPressed: toNewPage,
-            color: Colors.teal,
-            child: Text("Start the quizz",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
+            color: Colors.blue,
+            child: CustomText("Start the quizz",
+              factor: 1.5,
+              color: Colors.grey[900]
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
-    );
+      );
   }
 
   void toNewPage() {
